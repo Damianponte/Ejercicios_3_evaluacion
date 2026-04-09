a17f371cf01d7a6a85dae11ff43667bf29567c50
dami <damianponte@hotmail.es>
2026-04-09 13:05:29 +0200

    parte Usuario completa

--- src/Semana1/Contador/usuario/Usuario.java
+++ src/Semana1/Contador/usuario/Usuario.java
@@ -1,5 +1,31 @@
 package Semana1.Contador.usuario;
 
 public class Usuario {
+//Ejercicio 2:
+//Crea una clase Usuario con:
+//- atributo private String nombre
+//- atributo private static int totalUsuarios
+//En el constructor:
+//- asigna el nombre
+//- incrementa totalUsuarios
+//Añade:
+//- getter de nombre
+//- método static int getTotalUsuarios()
+//Desde main, crea varios usuarios y después de crearlos muestra el total (tomándolo de la clase Usuario).
 
+    private String nombre;
+    private static int totalUsuarios;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+        totalUsuarios++;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public static int getTotalUsuarios() {
+        return totalUsuarios;
+    }
 }
