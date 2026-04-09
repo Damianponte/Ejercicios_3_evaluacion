d1eb7329bec9ce4971782605d0a5a009e1775f4d
dami <damianponte@hotmail.es>
2026-04-09 12:51:32 +0200

    ejercicio completado

---
+++ src/Semana1/Contador/Contador.java
@@ -0,0 +1,23 @@
+package Semana1.Contador;
+
+public class Contador {
+    //Ejercicio 1:
+    //Crea una clase Semana1.Contador con:
+    //- un atributo static int contador
+    //- un método static void incrementar() que aumente el contador en 1
+    //- un método static int getContador()
+    //Desde una clase Principal1, llama varias veces a incrementar() y muestra el valor final.
+    // Recuerda que es todo static,
+    // NO tienes que crear una variable de tipo Semana1.Contador, sino usar Semana1.Contador.incrementar()
+
+
+     private static int contador;
+
+     public static void incrementar(){
+         contador++;
+     }
+
+     public static void getContador(){
+         System.out.println("contador:"+ contador);
+     }
+}
---
+++ src/Semana1/Contador/Main.java
@@ -0,0 +1,38 @@
+package Semana1.Contador;
+
+public class Main {
+    public static void main(String[] args) {
+        //Ejercicio 1:
+        //Crea una clase Semana1.Contador con:
+        //- un atributo static int contador
+        //- un método static void incrementar() que aumente el contador en 1
+        //- un método static int getContador()
+        //Desde una clase Principal1, llama varias veces a incrementar() y muestra el valor final.
+        // Recuerda que es todo static,
+        // NO tienes que crear una variable de tipo Semana1.Contador, sino usar Semana1.Contador.incrementar()
+
+
+
+       Contador.incrementar();
+       Contador.getContador();
+        Contador.incrementar();
+        Contador.getContador();
+
+        Contador.incrementar();
+        Contador.getContador();
+
+        Contador.incrementar();
+        Contador.getContador();
+
+        Contador.incrementar();
+        Contador.getContador();
+
+        Contador.incrementar();
+        Contador.getContador();
+
+
+
+
+
+    }
+}
