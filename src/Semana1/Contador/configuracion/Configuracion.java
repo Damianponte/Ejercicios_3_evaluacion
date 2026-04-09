29e6cf14ee3db26078617d0c779d10a59cba6fc0
dami <damianponte@hotmail.es>
2026-04-09 13:51:48 +0200

    emepzando ejercicio

--- src/Semana1/Contador/configuracion/Configuracion.java
+++ src/Semana1/Contador/configuracion/Configuracion.java
@@ -2,5 +2,12 @@ package Semana1.Contador.configuracion;
 
 public class Configuracion {
 
-    
+
+    //Crea una clase Configuracion con:
+    //- atributo private static String idioma = "ES"
+    //- método static void cambiarIdioma(String nuevoIdioma)
+    //- método static String getIdioma()
+    //Desde una clase con main, haz un método "saludar(String nombre)" que imprima "Hola nombre" si el idioma de la configuración es ES,
+    // y "Hello nombre" si el idioma de la configuración es "EN".
+    // Después, en el método "main", prueba a imprimir tras varias configuraciones.
 }
