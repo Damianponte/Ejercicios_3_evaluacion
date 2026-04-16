da8d21bddf9309514c9486b32284ada5795d53a7
dami <damianponte@hotmail.es>
2026-04-16 12:40:31 +0200

    empezando ejercicio parte main

---
+++ src/semana2/MainEstudiante.java
@@ -0,0 +1,10 @@
+package semana2;
+
+public class MainEstudiante {
+    // Crea una clase con main que tenga un conjunto de String con nombres de 5 asignaturas. En el main:
+    //- Crea 2 estudiantes, "John" y "Wick".
+    //- Recorre el array de asignaturas: para cada asignatura, pónsela a John y a Wick como aprobada o suspensa (haz un random de boolean);
+    // no tienen por qué tener la misma asignatura con la misma calificación.
+    //- Elige a uno de los dos estudiantes. Obtén una asignatura que haya suspendido. Apruébasela.
+    // Imprime de nuevo su listado de asignaturas aprobadas.
+}
